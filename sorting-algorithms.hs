ccab787019fec71fca61abf80f12c27501d67107
Mohamed <dtmohamed95@gmail.com>
2019-05-15 04:00:52 +0100

    selection sort implemented

--- sorting-algorithms.hs
+++ sorting-algorithms.hs
@@ -3,7 +3,7 @@
 -}
 
 {-
-    Insertion Sort: Insert an element x into a sorted list.
+    Insertion Sort: Take Elements one by one and insert them into the correct positions of the sorted list.
 -}
 
 
@@ -16,6 +16,33 @@ insert n (x:xs)
  | otherwise = x:(insert n xs)           -- otherwise recursivley iterate until you find a position you can put the element in.
 
 
+-- The actual algorithm making use of the above function 
+
 insertionSort :: Ord a => [a] -> [a]
 insertionSort [] = []
 insertionSort (x:xs)  = insert x (insertionSort xs)
+
+
+{-
+    Selection sort: find the smallest element and swapp it with the first element, repeat till you get to the end.
+-}
+
+-- deletes the first occurence from a list.
+
+delete :: Eq a => a -> [a] -> [a]
+delete n [] = []
+delete n (x:xs)
+ | n == x     = xs
+ | otherwise  = x:(delete n xs)
+
+
+selectionSort :: Ord a => [a] -> [a]
+selectionSort [] = []
+selectionSort xs = minVal:(selectionSort (delete minVal xs))  -- haskell you cant really swap so you just delete the first occurence here instead.
+                    where minVal = minimum xs
+
+
+
+
+
+
